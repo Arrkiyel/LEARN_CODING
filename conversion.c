1b634287ff45cc086b5a2e26fbf6c58e9fce61cd
Kiel <kiellelibang@gmail.com>
2025-11-22 12:23:07 +0800

    new

---
+++ conversion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+// Set the maximum possible score in the game to 500
+int maxScore = 500;
+
+// The actual score of the user
+int userScore = 423;
+
+/* Calculate the percantage of the user's score in relation to the maximum available score.
+Convert userScore to float to make sure that the division is accurate */
+float sum = (float) userScore / maxScore  * 100.0;
+
+// Print the percentage
+printf("The user's percentage is %.2f", sum);
+return 0;
+}
