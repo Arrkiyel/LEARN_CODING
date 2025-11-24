141d68878c232055e6fa48d3b06a7b393345216e
Kiel <kiellelibang@gmail.com>
2025-11-24 19:24:21 +0800

    constant test

---
+++ constant.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    const int mybirthyear = 2007;
+    const int secPermin = 60;
+
+    printf("My birth year is %d\n", mybirthyear);
+    printf("Each minute has %d seconds\n", secPermin);
+    return 0;
+}
